7b005ad2e8209426266e7524c13ebb570460da58
Aleksandr Mendeleev <aleksandrmendeleev581@gmail.com>
2024-08-04 23:40:14 +0300

    Workflow

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-// const manifestUrl = 'https://Avtoritetik.github.io/my_first_contract_frontend/tonconnect-manifest.json';
-const manifestUrl = "https://github.com/Avtoritetik/my_first_contract_frontend/blob/master/public/tonconnect-manifest.json"
+const manifestUrl = 'https://Avtoritetik.github.io/my_first_contract_frontend/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
